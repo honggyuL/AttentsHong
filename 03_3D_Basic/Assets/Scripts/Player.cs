5b4e6958f4d8f81fc555208515426c0eacbf4918
honggyuL <lhg2600@gmail.com>
2022-09-18 22:35:50 +0900

    Basic - 16일 마무리

--- 03_3D_Basic/Assets/Scripts/Player.cs
+++ 03_3D_Basic/Assets/Scripts/Player.cs
@@ -12,15 +12,24 @@ public class Player : MonoBehaviour
 
     float moveDir = 0.0f;
     float rotateDir = 0.0f;
+    bool isJumping = false;
+
+    GroundChecker checker;
 
     Rigidbody rigid;
+    Animator anim;
 
     PlayerInputActions inputActions;                // PlayerInputActions타입이고  inputActions 이름을 가진 변수를 선언.
+
+    
 
     private void Awake()
     {
         inputActions = new PlayerInputActions();    // 인스턴스 생성. 실제 메모리를 할당 받고 사용할 수 있도록 만드는 것.
         rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        checker = GetComponentInChildren<GroundChecker>();
+        checker.onGrounded += OnGround;
     }
 
     private void OnEnable()
@@ -45,6 +54,14 @@ public class Player : MonoBehaviour
     {
         Move();
         Rotate();
+
+        if (isJumping)
+        {
+            if( rigid.velocity.y < 0)
+            {
+                checker.gameObject.SetActive(true);
+            }
+        }
     }
 
     private void OnMoveInput(InputAction.CallbackContext context)
@@ -53,12 +70,17 @@ public class Player : MonoBehaviour
         //Debug.Log(input);
         moveDir = input.y;      // w : +1, s : -1   전진인지 후진인지 결정
         rotateDir = input.x;    // a : -1, d : +1   좌회전인지 우회전인지 결정
+
+        anim.SetBool("IsMove", !context.canceled);      // 이동키를 눌렀으면 true, 아니면 false
     }
 
     private void OnJumpInput(InputAction.CallbackContext _)
     {
-        // 플레이어의 위쪽 방향(up)으로 jumpPower만큼 즉시 힘을 추가한다.(질량 영향있음)
-        rigid.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+        if (!isJumping) // 점프 중이 아닐 때만 점프
+        {
+            isJumping = true;
+            JumpStart();
+        }
     }
 
     void Move()
@@ -75,5 +97,19 @@ public class Player : MonoBehaviour
 
         // Quaternion.Euler(0, rotateDir * rotateSpeed * Time.fixedDeltaTime, 0)    // x,z 축은 회전 없과 y축 기준으로 회전
         // Quaternion.AngleAxis(rotateDir * rotateSpeed * Time.fixedDeltaTime, transform.up)   // 플레이어의 y축기준으로 회전
+    }
+
+    void JumpStart()
+    {
+        // 플레이어의 위쪽 방향(up)으로 jumpPower만큼 즉시 힘을 추가한다.(질량 영향있음)
+        rigid.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+        isJumping = true;
+
+        checker.gameObject.SetActive(false);
+    }
+
+    void OnGround()
+    {
+        isJumping = false;
     }
 }
