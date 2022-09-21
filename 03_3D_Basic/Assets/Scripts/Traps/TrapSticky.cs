428d66ebbcc8a6043498fb91529c9a2aabd119b6
honggyuL <lhg2600@gmail.com>
2022-09-21 19:42:46 +0900

    Basic - Sticky 생성

---
+++ 03_3D_Basic/Assets/Scripts/Traps/TrapSticky.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TrapSticky : TrapBase
+{
+    public float speedDebuff = 0.1f;
+    public float duration = 3.0f;
+
+    float originalSpeed = 0.0f;
+    Player player = null;
+
+    protected override void TrapActivate(GameObject target)
+    {
+        if(player == null)
+        {
+            player = target.GetComponent<Player>();
+            originalSpeed = player.moveSpeed;
+            player.moveSpeed *= speedDebuff;
+        }
+        else
+        {
+            StopAllCoroutines();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(player == null)
+            {
+                StartCoroutine(ReleaseDebuff());
+            }
+        }
+    }
+
+    IEnumerator ReleaseDebuff()
+    {
+        yield return new WaitForSeconds(duration);
+        player.moveSpeed = originalSpeed;
+        originalSpeed = 0.0f;
+        player = null;
+    }
+}
